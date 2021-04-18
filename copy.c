386abf7edcc405cd25ca0b457415015d186b22d2
canoshiz <81651040+canoshiz@users.noreply.github.com>
2021-04-18 02:16:06 -0400

    Copy update

--- copy.c
+++ copy.c
@@ -3,12 +3,12 @@ int main(int argc, char *argv[]) {
 	FILE *source, *target;
 	source = fopen(argv[1], "rb");
 	target = fopen(argv[2], "wb");
-	int sizeI, sizeO;
 	char buffer[255];
+	int size;
 
 	while ( ! feof(source) ) {
-		sizeI = fread(buffer, 1, 100, source);
-		sizeO = fwrite(buffer, 1, sizeI, target);
+		size = fread(buffer, 1, 100, source);
+		fwrite(buffer, 1, size, target);
 	}
 
 	fclose(source);

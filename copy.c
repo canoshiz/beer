c56132d020658ab9947e92acde6513afa6aa39ad
canoshiz <81651040+canoshiz@users.noreply.github.com>
2021-05-17 11:45:42 -0400

    copy.c update

--- copy.c
+++ copy.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(int argc, char *argv[]) {
+int main(int *argc, char *argv[]) {
 	FILE *source, *target;
 	source = fopen(argv[1], "rb");
 	target = fopen(argv[2], "wb");
